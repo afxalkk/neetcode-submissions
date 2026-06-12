5a993c075fc10191caeaaa6e7f11637c03c156c0
afxalkk <44917087+afxalkk@users.noreply.github.com>
2026-06-12 20:55:39 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map()
+        let result = []
+        for(let i=0;i<nums.length;i++){
+            const t = target - nums[i]
+
+            if(map.has(t)){
+                return [map.get(t), i]
+            }
+
+            map.set(nums[i], i)
+        }
+    }
+}
