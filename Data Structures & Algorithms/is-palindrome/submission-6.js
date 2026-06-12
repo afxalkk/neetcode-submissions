88c2c11122e4d425e3427d63796bc789a7261489
afxalkk <44917087+afxalkk@users.noreply.github.com>
2026-06-12 21:08:06 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const formattedText = s.replace(/[/'$,?:. ]/g,'').toLowerCase()
+        let output = ``
+        for(let i=0;i<formattedText.length;i++){
+            output = `${formattedText[i]}${output}`
+        }
+        console.log(output, formattedText)
+        return output === formattedText
+    }
+}
