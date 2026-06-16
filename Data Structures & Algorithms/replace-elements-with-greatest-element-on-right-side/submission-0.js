27e95f20c7ab5b6b8d37749a1fd1cf733c5fde3b
afxalkk <44917087+afxalkk@users.noreply.github.com>
2026-06-16 22:51:32 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+
+    replaceElements(arr) {
+        let left = 0;
+        const findLargest = (start) => {
+            let largest = arr[start];
+            for (let i = start + 1; i < arr.length; i++) {
+                if (arr[i] > largest) {
+                    largest = arr[i];
+                }
+            }
+            return largest
+        };
+        while (left !== arr.length - 1) {
+            arr[left] =  findLargest(left + 1);
+            console.log(left, arr[left])
+            left++;
+        }
+        console.log(arr);
+        arr[arr.length-1] = -1
+        return arr;
+    }
+}
