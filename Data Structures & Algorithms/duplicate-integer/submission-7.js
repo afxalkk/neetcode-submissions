90a9721e2c61063be393ab6ca7ec7865ae09a597
afxalkk <44917087+afxalkk@users.noreply.github.com>
2026-06-28 12:57:08 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let isDuplicateExist = false;
+        const obj = new Map()
+        for (let i = 0; i < nums.length; i++) {
+            const currentNumber = nums[i]
+            if (obj.has(currentNumber)) {
+                isDuplicateExist = true;
+                break;
+            }
+            obj.set(currentNumber, true)
+
+        }
+        return isDuplicateExist;
+    }
+}
