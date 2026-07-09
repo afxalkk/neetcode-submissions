061d131f7979257ef6a4728e568e894c94ccf780
afxalkk <44917087+afxalkk@users.noreply.github.com>
2026-07-09 22:57:47 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        const obj = {};
+        let unique = null;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] in obj) {
+                obj[nums[i]] = obj[nums[i]] + 1;
+            } else {
+                obj[nums[i]] = 1;
+            }
+        }
+        for (const property in obj) {
+            console.log(property);
+            if(obj[property] === 1){
+                return property
+            }
+        }
+        // return unique;
+    }
+}
