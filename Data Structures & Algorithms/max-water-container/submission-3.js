8ee5cc024571cd7b1ba74d45c3f34a078849f6e0
afxalkk <44917087+afxalkk@users.noreply.github.com>
2026-07-14 19:13:27 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let area = 0;
+        for (let i = 0; i < heights.length; i++) {
+            for (let j = i + 1; j < heights.length; j++) {
+                const minimum = Math.min(heights[i], heights[j]) * (j-i)
+                area = Math.max(minimum, area)
+            }
+        }
+
+        return area
+    }
+}
